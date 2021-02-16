4a6d6b41571250c8f97187bc69b99d4862316902
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-16 12:24:52 -0300

    methods java

--- src/br/dev/rvz/Applicaton.java
+++ src/br/dev/rvz/Applicaton.java
@@ -2,11 +2,12 @@ package br.dev.rvz;
 
 public class Applicaton {
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        calculateScore(true,8, 5, 100);
+        System.out.println("");
+        calculateScore(true, 10000, 8, 200);
+    }
 
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (score < 500) {
             System.out.println("Your socre is 5000");
         } else if (score < 1000 && score >= 5000) {
@@ -18,14 +19,6 @@ public class Applicaton {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             System.out.println("Yor final score was " + finalScore);
-        }
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        if (gameOver) {
-            System.out.println("Your final socre was " + (score + (levelCompleted * bonus)));
         }
     }
 }
