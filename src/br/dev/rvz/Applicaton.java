a552c151144fd9a42ab0c6bf4541b552a612579e
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-16 12:39:10 -0300

    refactor method calculatescore

--- src/br/dev/rvz/Applicaton.java
+++ src/br/dev/rvz/Applicaton.java
@@ -2,23 +2,17 @@ package br.dev.rvz;
 
 public class Applicaton {
     public static void main(String[] args) {
-        calculateScore(true,8, 5, 100);
+        System.out.println("sua pontução final foi: " + calculateScore(true,8, 5, 100));
         System.out.println("");
-        calculateScore(true, 10000, 8, 200);
+        System.out.println("sua pontuação final foi: " + calculateScore(true, 10000, 8, 200));;
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-        if (score < 500) {
-            System.out.println("Your socre is 5000");
-        } else if (score < 1000 && score >= 5000) {
-            System.out.println("Your score was less than 1000");
-        }else {
-            System.out.println("Go here");
-        }
-
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = -1;
         if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Yor final score was " + finalScore);
+            finalScore = score + (levelCompleted * bonus);
+            return finalScore;
         }
+        return finalScore;
     }
 }
