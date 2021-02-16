095ca1e7b6b36d42588691148083f394f227e1f5
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-16 12:19:55 -0300

    If and else e else if

--- src/br/dev/rvz/Applicaton.java
+++ src/br/dev/rvz/Applicaton.java
@@ -1,11 +1,31 @@
 package br.dev.rvz;
 
-import java.util.Scanner;
-
 public class Applicaton {
     public static void main(String[] args) {
-        double pounds = new Scanner(System.in).nextDouble();
-        double kg = pounds * 0.45d;
-        System.out.println(pounds + "Libras é igual a " + kg + " kilogramas");
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 500) {
+            System.out.println("Your socre is 5000");
+        } else if (score < 1000 && score >= 5000) {
+            System.out.println("Your score was less than 1000");
+        }else {
+            System.out.println("Go here");
+        }
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Yor final score was " + finalScore);
+        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver) {
+            System.out.println("Your final socre was " + (score + (levelCompleted * bonus)));
+        }
     }
 }
