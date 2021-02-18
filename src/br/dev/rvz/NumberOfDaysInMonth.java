fee45ee4e495915c240f9b181ab0f3eb2e92d2d5
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-18 08:28:48 -0300

    Get number the days per month

--- src/br/dev/rvz/NumberOfDaysInMonth.java
+++ src/br/dev/rvz/NumberOfDaysInMonth.java
@@ -7,4 +7,28 @@ public class NumberOfDaysInMonth {
         }
         return false;
     }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12) {
+            return -1;
+        } else if (year < 1 || year > 9999) {
+            return -1;
+        } else {
+            if (month == 1 || month == 3 || month == 5
+                    || month == 7 || month == 8 || month == 12
+            || month == 10) {
+                return 31;
+            } else if (month == 2) {
+                if (isLeapYear(year)) {
+                    return 29;
+                }
+                return 28;
+            } else if (month == 4 || month == 6
+                    || month == 9 || month == 11) {
+                return 30;
+            } else {
+                return -1;
+            }
+        }
+    }
 }
